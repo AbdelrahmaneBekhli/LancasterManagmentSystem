06b74d5e21560395ea02185ed849ba09e1714514
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2024-04-14 02:08:06 +0100

    implemented the GUI to function

--- src/main/java/com/example/lancastermanagmentsystem/AdminMenusMenuController.java
+++ src/main/java/com/example/lancastermanagmentsystem/AdminMenusMenuController.java
@@ -3,6 +3,7 @@ package com.example.lancastermanagmentsystem;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
@@ -10,13 +11,63 @@ import java.util.ResourceBundle;
 
 public class AdminMenusMenuController implements Initializable {
     @FXML
-    Button backButton;
+    Button dashboard, menu, statistics, supplier, staff, logout;
+
+    @FXML
+    Text user, dateTime;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        backButton.setOnAction(e -> {
+        user.setText(LoginController.user);
+        LoginPage.setTime(dateTime);
+
+        ButtonImage dashboardButtonImage = new ButtonImage(dashboard, "Dashboard");
+        ButtonImage menuButtonImage = new ButtonImage(menu, "Menu", true);
+        ButtonImage graphButtonImage = new ButtonImage(statistics, "Statistics");
+        ButtonImage supplierButtonImage = new ButtonImage(supplier, "Supplier");
+        ButtonImage staffButtonImage = new ButtonImage(staff, "Staff");
+        ButtonImage logoutButtonImage = new ButtonImage(logout, "Logout");
+
+        dashboardButtonImage.setGraphics();
+        menuButtonImage.setGraphics();
+        graphButtonImage.setGraphics();
+        supplierButtonImage.setGraphics();
+        staffButtonImage.setGraphics();
+        logoutButtonImage.setGraphics();
+
+
+
+        dashboard.setOnAction(e -> {
             try {
-                LoginPage.setAdminPage();
+                LoginPage.setDashboardPage();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        statistics.setOnAction(e -> {
+            try {
+                LoginPage.setStatisticsPage();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        supplier.setOnAction(e -> {
+            try {
+                LoginPage.setSupplierPage();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        staff.setOnAction(e -> {
+            try {
+                LoginPage.setStaffPage();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        logout.setOnAction(e -> {
+            try {
+                LoginPage.setLoginPage();
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
